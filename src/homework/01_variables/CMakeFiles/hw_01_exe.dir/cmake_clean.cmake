32c93c011f13706bfacbc31afdcd0227cfefcfdf
Grecia1813 <grecia.bueno@g.austincc.edu>
2020-09-18 03:14:09 +0000

    Homework_01

---
+++ src/homework/01_variables/CMakeFiles/hw_01_exe.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw_01_exe.dir/main.cpp.o"
+  "CMakeFiles/hw_01_exe.dir/variables.cpp.o"
+  "hw_01_exe.pdb"
+  "hw_01_exe"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw_01_exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/homework/01_variables/main.cpp
+++ src/homework/01_variables/main.cpp
@@ -1,5 +1,36 @@
-﻿//write include statements
+//write include statements
+#include "variables.h"
+#include<iostream>
 
+using std::cout; using std::cin;
+
+
+int main() 
+{
+  double meal_amount;
+  double tip_rate;
+  double tip_amount;
+  double tax_amount;
+  double total;
+
+  cout << "\n";
+  cout << "Enter the meal price: ";
+  cin >> meal_amount;
+  tax_amount = get_sales_tax_amount(meal_amount);
+
+  cout << "Enter tip percentage: ";
+  cin >> tip_rate;
+  tip_amount = get_tip_amount(meal_amount, tip_rate);
+
+  cout << "\n";
+
+  total = tip_amount + tax_amount + meal_amount;
+  cout << "Meal amount: $" << meal_amount << "\n";
+  cout << "Sales Tax: $" << tax_amount << "\n";
+  cout << "Tip Amount $" << tip_amount << "\n";
+  cout << "Your total is: $" << total << "\n";
+
+}
 
 //write namespace using statement for cout
 
@@ -7,9 +38,4 @@
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
-int main()
-{
-	
 
-	return 0;
-}
